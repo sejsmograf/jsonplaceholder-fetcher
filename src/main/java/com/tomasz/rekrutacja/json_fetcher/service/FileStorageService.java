6585fb362d5fc0195a1ca42f452ad2ee280cfa97
Tomasz Mroczko <tomekmr2001@gmail.com>
2025-04-07 14:00:42 +0200

    remvoe unnecesssary line

--- src/main/java/com/tomasz/rekrutacja/json_fetcher/service/FileStorageService.java
+++ src/main/java/com/tomasz/rekrutacja/json_fetcher/service/FileStorageService.java
@@ -2,6 +2,7 @@ package com.tomasz.rekrutacja.json_fetcher.service;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -11,10 +12,10 @@ import org.springframework.stereotype.Service;
 public class FileStorageService implements StorageService {
 
 	public void saveContentToFile(String content, Path filePath) {
-		Path dir = filePath.getParent();
-
 		try {
 			Files.writeString(filePath, content);
+		} catch (AccessDeniedException e) {
+			throw new RuntimeException("Access denied attempting to access " + filePath.toString());
 		} catch (IOException e) {
 			throw new UncheckedIOException(e);
 		}
